c61ba70aec9e0831a23f289e6989c48c8b21c08e
saul <dlathf3210@naver.com>
2023-09-26 14:53:32 +0900

    create TokenProvider

--- src/main/java/com/example/security_jwt_pr/jwt/TokenProvider.java
+++ src/main/java/com/example/security_jwt_pr/jwt/TokenProvider.java
@@ -1,7 +1,21 @@
 package com.example.security_jwt_pr.jwt;
 
+import com.example.security_jwt_pr.domain.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Set;
 
 @RequiredArgsConstructor
 @Service
@@ -9,9 +23,62 @@ public class TokenProvider {
 
     private final JwtProperties jwtProperties;
 
-//    public String generateToken(){
-//
-//    }
+    public String makeToken(Date expiry, User user){
+        Date now = new Date();
+
+        return Jwts.builder()
+                .setHeaderParam(Header.TYPE, Header.JWT_TYPE) // 헤더 typ : JWT
+                .setIssuer(jwtProperties.getIssuer())
+                .setIssuedAt(now)
+                .setExpiration(expiry)
+                .setSubject(user.getEmail())
+                .claim("id", user.getId())
+                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
+                .compact();
+    }
+
+    // 토큰 유효성 검증 메소드
+    public boolean validToken(String token){
+        try {
+            Jwts.parser()
+                    .setSigningKey(jwtProperties.getSecretKey()) // 비밀값으로 복호화
+                    .parseClaimsJws(token);
+            return true;
+        }catch (Exception e){ // 복호화 과정에서 에러가 나지 않으면 유효한 토큰
+            return false;
+        }
+                
+    }
+
+    // 토큰 기반으로 인증 정보를 가져오는 메서드
+    public Authentication getAuthentication(String token){
+        Claims claims = getClaims(token);
+        Set<SimpleGrantedAuthority> authorities = Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+
+        return new UsernamePasswordAuthenticationToken(
+                new org.springframework.security.core.userdetails.User(claims.getSubject(), "", authorities),
+                token,
+                authorities
+        );
+    }
+
+
+
+    public Long getUserId(String token){
+        Claims claims = getClaims(token);
+        return claims.get("id", Long.class);
+    }
+
+
+    private Claims getClaims(String token){
+        return Jwts.parser()
+                .setSigningKey(jwtProperties.getSecretKey())
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+
+
 
 
 }
