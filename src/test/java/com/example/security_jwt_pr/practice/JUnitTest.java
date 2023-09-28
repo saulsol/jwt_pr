927cf91949ea125d67f0ce280cc9ebc9ca16cb35
saul <dlathf3210@naver.com>
2023-09-28 16:53:59 +0900

    add test method

--- src/test/java/com/example/security_jwt_pr/practice/JUnitTest.java
+++ src/test/java/com/example/security_jwt_pr/practice/JUnitTest.java
@@ -1,4 +1,24 @@
 package com.example.security_jwt_pr.practice;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class JUnitTest {
+
+    @DisplayName("1 더하기 2는 3이다.")
+    @Test
+    public void junitTest(){
+        int a = 1;
+        int b = 2;
+
+        int sum = 3;
+
+        Assertions.assertEquals(a+b, sum);
+    }
+
+
+
 }
---
+++ src/test/java/com/example/security_jwt_pr/practice/JunitCycleTest.java
@@ -0,0 +1,47 @@
+package com.example.security_jwt_pr.practice;
+
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+public class JunitCycleTest {
+
+
+    @BeforeAll // 전체 테스트를 시작하기 전에 1회 실행하므로 메서드는 static으로 선언
+    static void beforeAll(){
+        System.out.println("@BeforeAll");
+    }
+
+    @BeforeEach // 테스트 케이스를 시작하기 전마다 실행
+    public void beforeEach(){
+        System.out.println("@BeforeEach");
+    }
+
+    @Test
+    public void test1(){
+        System.out.println("test1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("test2");
+    }
+
+    @Test
+    public void test3(){
+        System.out.println("test3");
+    }
+
+    @AfterAll // 전체 테스트를 마치고 종료하기 전에 1회 실행하므로 메서드는 static으로 선언
+    static void afterAll(){
+        System.out.println("@AfterAll");
+    }
+
+    @AfterEach // 테스트 케이스를 종료하기 전마다 실행
+    public void afterEach(){
+        System.out.println("@AfterEach");
+    }
+
+
+}
